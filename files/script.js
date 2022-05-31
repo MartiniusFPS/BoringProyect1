1a8947804befb2271af7be67b40755fb097e738f
MartiniusFPS <106590823+MartiniusFPS@users.noreply.github.com>
2022-05-31 12:15:36 +0200

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,39 @@
+var page=1;
+function nextPage(){
+    if (page<250) {
+        var botP = document.getElementById("prevB");
+        page++;
+        var elm = document.createElement("iframe");
+        var dir = "files/NameWIP.pdf#view=fit&navpanes=0&toolbar=0&scrollbar=0&page="+page;
+        var ifr = document.getElementById('txt');
+        var par = ifr.parentElement;
+        ifr.remove();
+        elm.src = dir;
+        elm.id ="txt";
+        par.appendChild(elm);
+        botP.disabled=false;
+    }
+}
+function prevPage(){
+    var botP = document.getElementById("prevB");
+    if (page>1) {
+        page--;
+        var elm = document.createElement("iframe");
+        var dir = "files/NameWIP.pdf#view=fit&navpanes=0&toolbar=0&scrollbar=0&page="+page;
+        var ifr = document.getElementById('txt');
+        var par = ifr.parentElement;
+        ifr.remove();
+        elm.src = dir;
+        elm.id ="txt";
+        par.appendChild(elm);
+    }
+    if (page==1) {
+        botP.disabled=true;
+    }
+}
+function start(){
+    var dir = "files/NameWIP.pdf#view=fit&navpanes=0&toolbar=0&scrollbar=0&page="+page;
+    document.getElementById('txt').src= dir;
+    prevPage();
+}
+window.onload = start;
